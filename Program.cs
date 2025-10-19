7ad47c90c1845cea6b733c95d7b4b98fde104f67
maljaethereal <121117752+MaksimOlienyk@users.noreply.github.com>
2025-10-19 18:10:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿abstract class Prototype { public abstract Prototype Clone(); }
+
+class ConcretePrototype : Prototype
+{
+    public int Data;
+    public ConcretePrototype(int data) { Data = data; }
+    public override Prototype Clone() => (Prototype)MemberwiseClone();
+}
+
+class Program
+{
+    static void Main()
+    {
+        var p1 = new ConcretePrototype(5);
+        var p2 = (ConcretePrototype)p1.Clone();
+        Console.WriteLine(p2.Data);
+    }
+}
